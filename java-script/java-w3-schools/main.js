6129a09807e852bcc04c7cd59101499e90b2a439
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-03 13:49:48 -0400

    A whole lot over many days, (forgot to save dayly :P)

---
+++ java-script/java-w3-schools/main.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+console.warn('This is a warning');
+console.error('This is an error');
+
+function tag() {
+    document.getElementById("tag").innerHTML = "You Clicked Me :O";
+}
