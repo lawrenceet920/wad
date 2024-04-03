8831c5855dcedb06cbe325dd7e5bf336b5e8f3b1
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-03 13:58:55 -0400

    Java Template

---
+++ java-script/java-template/main.js
@@ -0,0 +1,12 @@
+// Ethan Lawrence
+// March 3 2024
+// Script For JavaScript Template
+
+// This is a single line JavaScript Template
+
+/* You can also
+add comments to 
+Your javaScript File  that
+Span more then
+One line
+*/
