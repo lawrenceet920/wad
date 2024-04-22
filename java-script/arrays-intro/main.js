9f1d8db9a68c854b0905f7599ae959fed78691b4
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-04-22 14:12:56 -0400

    Complete

--- java-script/arrays-intro/main.js
+++ java-script/arrays-intro/main.js
@@ -3,14 +3,14 @@
 // Array intro
 document.getElementById("btn").addEventListener("click", fruity);
 
-function fruity() {
-// Input
+// Imput
 const fruits = ["Apple", "Cherry", "Orange"]; 
 console.log(`Initial 'fruits' array = ${fruits}`);
 fruits.push("Banana");
 fruits.push("Peach", "Pear");
 console.log(fruits);
 
+function fruity() {
 // Process
 // Create a variable - Round down - random number - between 0 and the length of array fruits 
 const randomIndex =  Math.floor((Math.random() * fruits.length));
